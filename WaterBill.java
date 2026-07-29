5fdcc05604f0b9d81563561c9311244d36ca4b6c
amulya y m <amulyabedag@gmail.com>
2026-07-29 22:44:23 +0530

    objectexample2

---
+++ WaterBill.java
@@ -0,0 +1,27 @@
+class WaterBill{
+	String subDivision;
+	String rrNumber;
+	int consNo;
+	String billNo;
+	String rdgDate;
+	String dueDate;
+	int presRdg;
+	int prevRdg;
+	int consumptionLtrs;
+	double waterCharges;
+	double meterCharges;
+	double sanitaryCharges;
+	double scForBorewell;
+	double otherCharges;
+	double arrears;
+	double interest;
+	double totalAmount;
+	double advAmount;
+	double netAmountDue;
+	String amountInWords;
+	
+	
+	
+
+
+}
